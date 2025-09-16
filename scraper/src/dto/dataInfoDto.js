ebc0f5541eec09da6e9632ee536266a15b39b7f3
Joao Lima <jplima1853@gmail.com>
2025-09-16 11:20:56 -0300

    objeto de tranferência de dados para facilitar comunicação entre os módulos

---
+++ scraper/src/dto/dataInfoDto.js
@@ -0,0 +1,13 @@
+class DataInfo {
+    constructor(mark, model, cpu, mem, storage) {
+        this.mark = mark;
+        this.model = model;
+        this.cpu = cpu;
+        this.storage = storage;
+        this.mem = mem;
+    }
+}
+
+module.exports = {
+    DataInfo
+};
